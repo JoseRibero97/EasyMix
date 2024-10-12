431d7810341a94b8673b63c92acf8ed81a2e2680
Duvan0622 <geiner2201840@correo.uis.edu.co>
2024-10-12 13:20:00 -0500

    version base

---
+++ easymix/src/main/java/com/uis/easymix/EasymixApplication.java
@@ -0,0 +1,13 @@
+package com.uis.easymix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasymixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasymixApplication.class, args);
+	}
+
+}
---
+++ easymix/src/test/java/com/uis/easymix/EasymixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uis.easymix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasymixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
