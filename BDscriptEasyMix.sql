faa8a5333251c34f83ce2c2f780b61d3ae83ae8a
Jose David Ribero Arciniegas <jodariar970@gmail.com>
2024-10-23 19:26:23 -0500

    Cambios en el script de la BD

--- BDscriptEasyMix.sql
+++ BDscriptEasyMix.sql
@@ -3,102 +3,102 @@ USE easyMix;
 
 -- Tabla Usuario
 CREATE TABLE usuario (
-    usuario_id INT AUTO_INCREMENT,
-    usuario_nombre VARCHAR(255) NOT NULL,
-    usuario_email VARCHAR(255) NOT NULL UNIQUE,
+    id INT AUTO_INCREMENT,
+    nombre VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
     contrasena VARCHAR(255) NOT NULL,
-    PRIMARY KEY (usuario_id)
+    PRIMARY KEY (id)
 );
 
 -- Tabla Proveedor
 CREATE TABLE proveedor(
-	proveedor_id INT auto_increment,
-    proveedor_nombre VARCHAR(255) NOT NULL,
-    proveedor_contacto varchar(255) NOT NULL,
-    PRIMARY KEY(proveedor_id)
+	id INT auto_increment,
+    nombre VARCHAR(255) NOT NULL,
+    contacto varchar(255) NOT NULL,
+    PRIMARY KEY(id)
 );
 
 -- Tabla Producto
 CREATE TABLE producto (
-    producto_id INT AUTO_INCREMENT,
-    producto_nombre VARCHAR(255) NOT NULL,
+    id INT AUTO_INCREMENT,
+    nombre VARCHAR(255) NOT NULL,
     descripcion VARCHAR(255),
     tipo VARCHAR(255),
     precio DOUBLE,
     marca VARCHAR(255),
     presentacion VARCHAR(255),
-    cantidad_presentacion DOUBLE,
-    medida_cantidad VARCHAR(255),
+    cantidad DOUBLE,
+    medida VARCHAR(255),
     usuario_id INT,
     proveedor_id INT,
-    PRIMARY KEY (producto_id),
-    FOREIGN KEY (usuario_id) REFERENCES usuario(usuario_id),
-    FOREIGN KEY (proveedor_id) REFERENCES proveedor(proveedor_id)
+    PRIMARY KEY (id),
+    FOREIGN KEY (usuario_id) REFERENCES usuario(id),
+    FOREIGN KEY (proveedor_id) REFERENCES proveedor(id)
 );
 
 -- Tabla Receta
 CREATE TABLE receta (
-    receta_id INT AUTO_INCREMENT,
-    nombre_producto_final VARCHAR(255) NOT NULL,
-    PRIMARY KEY (receta_id)
+    id INT AUTO_INCREMENT,
+    nombre VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
 );
 
 -- Tabla Receta_Ingredientes
 CREATE TABLE receta_ingredientes (
-	Receta_Ingredientes_id INT AUTO_INCREMENT,
+	id INT AUTO_INCREMENT,
 	producto_id INT,
     receta_id INT,
     cantidad DOUBLE NOT NULL,
-    unidad_medida VARCHAR(255) NOT NULL,
-    PRIMARY KEY (Receta_Ingredientes_id),
-    FOREIGN KEY (receta_id) REFERENCES receta(receta_id),
-    FOREIGN KEY (producto_id) REFERENCES producto(producto_id)
+    medida VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (receta_id) REFERENCES receta(id),
+    FOREIGN KEY (producto_id) REFERENCES producto(id)
 );
 
 -- Tabla Produccion
 CREATE TABLE produccion (
-    produccion_id INT AUTO_INCREMENT,
+    id INT AUTO_INCREMENT,
     cantidad_producida INT NOT NULL,
     fecha_inicio DATE NOT NULL,
     fecha_fin DATE NOT NULL,
     estado VARCHAR(255),
     producto_id INT,
-    PRIMARY KEY (produccion_id),
-    FOREIGN KEY (producto_id) REFERENCES producto(producto_id)
+    PRIMARY KEY (id),
+    FOREIGN KEY (producto_id) REFERENCES producto(id)
 );
 
 -- Tabla Produccion_Insumos
 CREATE TABLE produccion_insumos (
-	produccion_Insumo_Id INT auto_increment,
+	id INT auto_increment,
     produccion_id INT,
     producto_id INT,
     cantidad_requerida DOUBLE NOT NULL,
     suficiente BOOLEAN NOT NULL,
-    PRIMARY KEY (produccion_Insumo_Id),
-    FOREIGN KEY (produccion_id) REFERENCES produccion(produccion_id),
-    FOREIGN KEY (producto_id) REFERENCES producto(producto_id)
+    PRIMARY KEY (id),
+    FOREIGN KEY (produccion_id) REFERENCES produccion(id),
+    FOREIGN KEY (producto_id) REFERENCES producto(id)
 );
 
 -- Tabla Inventario
 CREATE TABLE inventario (
-    inventario_id INT AUTO_INCREMENT,
+    id INT AUTO_INCREMENT,
     cantidad_disponible INT NOT NULL,
     fecha_registro DATE NOT NULL,
     producto_id INT,
-    PRIMARY KEY (inventario_id),
-    FOREIGN KEY (producto_id) REFERENCES producto(producto_id)
+    PRIMARY KEY (id),
+    FOREIGN KEY (producto_id) REFERENCES producto(id)
 );
 
 use easyMix;
 
-INSERT INTO usuario (usuario_nombre, usuario_email, contrasena) VALUES
+INSERT INTO usuario (nombre, email, contrasena) VALUES
 ('Ana García', 'ana.garcia@example.com', 'AnaG1234'),
 ('Carlos Pérez', 'carlos.perez@example.com', 'Carlo5678'),
 ('Lucía Rodríguez', 'lucia.rodriguez@example.com', 'Luci7890'),
 ('Javier López', 'javier.lopez@example.com', 'Javie4321'),
 ('María Torres', 'maria.torres@example.com', 'MariA0987');
 
-INSERT INTO proveedor (proveedor_nombre, proveedor_contacto) VALUES
+INSERT INTO proveedor (nombre, contacto) VALUES
 ('Alimentos Naturales S.A.', 'Tel: 123456789, Email: contacto@alimentosnaturales.com'),
 ('NutriPet S.L.', 'Tel: 987654321, Email: ventas@nutripet.com'),
 ('Fábrica de Comida Animal, S.A.', 'Tel: 234567890, Email: info@fabricaanimal.com'),
@@ -106,7 +106,7 @@ INSERT INTO proveedor (proveedor_nombre, proveedor_contacto) VALUES
 ('Granos y Semillas, S.A.', 'Tel: 456789012, Email: ventas@granosysemillas.com');
 
 -- Inserción de Datos de Productos (Insumos)
-INSERT INTO producto (producto_nombre, descripcion, tipo, precio, marca, presentacion, cantidad_presentacion, medida_cantidad, usuario_id, proveedor_id) VALUES
+INSERT INTO producto (nombre, descripcion, tipo, precio, marca, presentacion, cantidad, medida, usuario_id, proveedor_id) VALUES
 ('Pollo Deshidratado', 'Proteína de pollo deshidratada para mascotas', 'insumo', 20.00, 'PetNutrient', 'Bolsa', 5, 'kg', 1, 1),
 ('Arroz Integral', 'Arroz integral para la mezcla de alimentos', 'insumo', 1.50, 'GranosAndes', 'Saco', 25, 'kg', 2, 2),
 ('Zanahoria Deshidratada', 'Zanahoria deshidratada en trozos', 'insumo', 15.00, 'VeggiePets', 'Bolsa', 2, 'kg', 3, 3),
@@ -115,14 +115,14 @@ INSERT INTO producto (producto_nombre, descripcion, tipo, precio, marca, present
 
 
 -- Inserción de Datos de Recetas (Productos Finales)
-INSERT INTO receta (nombre_producto_final) VALUES
+INSERT INTO receta (nombre) VALUES
 ('Croquetas de Pollo y Arroz'),
 ('Snacks de Zanahoria y Salmón'),
 ('Comida Húmeda para Perros con Pollo'),
 ('Alimento Seco de Pescado'),
 ('Galletas de Pollo y Zanahoria');
 
-INSERT INTO receta_ingredientes (producto_id, receta_id, cantidad, unidad_medida) VALUES
+INSERT INTO receta_ingredientes (producto_id, receta_id, cantidad, medida) VALUES
 (1, 1, 3.0, 'kg'),  -- Pollo Deshidratado
 (2, 1, 2.0, 'kg'),  -- Arroz Integral
 (3, 2, 0.5, 'kg'),  -- Zanahoria Deshidratada
@@ -151,15 +151,10 @@ INSERT INTO inventario (cantidad_disponible, fecha_registro, producto_id) VALUES
 (300, '2024-10-10', 5); -- Galletas de Pollo y Zanahoria
 
 -- Inserción de Productos Producidos en la Tabla Producto
-INSERT INTO producto (producto_nombre, descripcion, tipo, precio, marca, presentacion, cantidad_presentacion, medida_cantidad, usuario_id, proveedor_id) VALUES
+INSERT INTO producto (nombre, descripcion, tipo, precio, marca, presentacion, cantidad, medida, usuario_id, proveedor_id) VALUES
 ('Croquetas de Pollo y Arroz', 'Alimento balanceado para perros con pollo y arroz', 'producido', 63.00, 'PetNutrient', 'Unidad', 1, 'kg', 1, 1),
 ('Snacks de Zanahoria y Salmón', 'Deliciosos snacks de zanahoria y salmón', 'producido', 10.50, 'VeggiePets', 'Unidad', 1, 'kg', 2, 2),
 ('Comida Húmeda para Perros', 'Comida húmeda de pollo para perros', 'producido', 46.00, 'HealthyFur', 'Unidad', 1, 'kg', 3, 3),
 ('Alimento Seco de Pescado', 'Alimento seco de pescado rico en omega 3', 'producido', 69.00, 'SeafoodDelight', 'Unidad', 1, 'kg', 4, 4),
 ('Galletas de Pollo y Zanahoria', 'Galletas saludables de pollo y zanahoria', 'producido', 27.50, 'PetNutrient', 'Unidad', 1, 'kg', 5, 5);
-
-
-
-
-
 
